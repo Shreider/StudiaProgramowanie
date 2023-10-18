ff9e3f078ee5c0514a018c021ddb130d6daae60e
Shreider <aditur00@gmail.com>
2023-10-18 12:42:49 +0200

    sex

--- 01/index.ts
+++ 01/index.ts
@@ -2,6 +2,6 @@ let exampleNumber : number = 5;
 const constExampleNumber : number = 5;
 let helloMessage : string = 'Hello world';
 
-exampleNumber = 2;
+exampleNumber = 1;
 
 console.log(helloMessage);
