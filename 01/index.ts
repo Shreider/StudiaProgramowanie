c2d1d613c8531e5a31152e51a44dd1830ab30244
Shreider <aditur00@gmail.com>
2023-10-18 12:43:19 +0200

    01

--- 01/index.ts
+++ 01/index.ts
@@ -2,6 +2,6 @@ let exampleNumber : number = 5;
 const constExampleNumber : number = 5;
 let helloMessage : string = 'Hello world';
 
-exampleNumber = 1;
+exampleNumber = 2;
 
 console.log(helloMessage);
